60291162278ab7ca9c1e286eace52aad569fff2e
Fortune Igbinobaro <igbinobaroosadolor@gmail.com>
2023-10-04 12:21:34 +0100

    Add automated tests for the web application using Selenium

---
+++ selenium_test.js
@@ -0,0 +1,28 @@
+const { Builder, By } = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+
+(async function example() {
+    const options = new chrome.Options();
+    options.addArguments('headless');
+
+    const driver = await new Builder()
+        .forBrowser('chrome')
+        .setChromeOptions(options)
+        .build();
+
+    try {
+        await driver.get('file://' + __dirname + '/index.html');
+
+        const button = await driver.findElement(By.tagName('button'));
+        await button.click();
+
+        // Wait for an alert to appear and accept it
+        const alert = await driver.switchTo().alert();
+        await alert.accept();
+
+        // Add more tests as needed
+
+    } finally {
+        await driver.quit();
+    }
+})();
